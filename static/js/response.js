bcd57f7e561080239760e50fe7262b077d01f3b5
shalom-geek-ng <114221178+shalom-geek-ng@users.noreply.github.com>
2022-12-09 11:56:58 +0100

    Add files via upload

---
+++ static/js/response.js
@@ -0,0 +1,8 @@
+const responseObj = {
+    hello: "Hey ! How are you doing? how may I be of help. Do you want to crate a website?" ,
+    hey: "Hey! What's up, how may I be of help. Do you want to create a website?" ,
+    yes:"We are happy, Contact Oluwafemi Shalom on 08166649897",
+    no: "Okay u may later decide to create one, well this is our number 0816649897",
+    today: new Date().toDateString(),
+    time:new Date().toLocaleDateString(),
+}
